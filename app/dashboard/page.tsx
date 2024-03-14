4ff40a404955341c684a404cdae5c4f3c1c4a0d8
VulcanWM <logmedha09@gmail.com>
2024-03-14 21:30:01 +0000

    added bets to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,18 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import { get_user_from_email } from "@/lib/database";
+import { get_user_from_email, get_acceptor_bets, get_starter_bets } from "@/lib/database";
+import { networkInterfaces } from 'os';
+
+type Bet = {
+  title: String,
+  desc: String,
+  amount: Number,
+  starter: String,
+  acceptor: String,
+  guarantor: String,
+  status: Boolean
+}
 
 export default async function Home() {
   const authUser = await getServerSession(authOptions);
@@ -13,7 +24,8 @@ export default async function Home() {
   if (userDoc == false){
     redirect("/create-account")
   }
-
+  const acceptorBets = await get_acceptor_bets(userDoc.username);
+  const starterBets = await get_starter_bets(userDoc.username);
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
@@ -28,6 +40,22 @@ export default async function Home() {
         <p>{userDoc.draws} draws</p>
         <p>{userDoc.currency} currency</p>
       </div>
+      {starterBets.map((bet: Bet) =>
+        <>
+          {bet.status == false ? 
+          <p>You have started a bet with {bet.acceptor} for {bet.title}. You are waiting for them to accept.</p>
+          :
+          <p>You are doing a bet with {bet.acceptor} for {bet.title}</p>}
+        </>
+      )}
+      {acceptorBets.map((bet: Bet) =>
+        <>
+          {bet.status == false ? 
+          <p>{bet.starter} has requested to do a bet with you about {bet.title} for {String(bet.amount)}. Accept or deny</p>
+          :
+          <p>You are doing a bet with {bet.starter} for {bet.title}</p>}
+        </>
+      )}
     </section>
   )
 }
