119456fde9c9a0c09e71f412db019e1056e0ec28
VulcanWM <logmedha09@gmail.com>
2024-03-14 21:48:03 +0000

    added `createBetFunction()`

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { create_user, get_user_from_email } from "@/lib/database"
+import { create_user, get_user_from_email, create_bet } from "@/lib/database"
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
@@ -17,5 +17,27 @@ export async function createUserFunction(prevState: { message: string } | { mess
         }
         const func = await create_user(username, email as string)
         return {message: func}
+    }
+}
+
+export async function createBetFunction(prevState: { message: string } | { message: boolean }, formData: FormData) {
+    const acceptor = formData.get("acceptor") as string;
+    const guarantor = formData.get("guarantor") as string;
+    const amount = parseInt(formData.get("amount") as string)
+    const title = formData.get("title") as string;
+    const desc = formData.get("desc") as string;
+    const authUser = await getServerSession(authOptions);
+    const email = authUser?.user?.email || null
+    if (email == null){
+        return {message: "You are not logged in"}
+    } else {
+        const user = await get_user_from_email(email)
+        if (user == false){
+            return {message: "You do not have an account"}
+        } else {
+            const starter = user.username;
+            const func = await create_bet(starter, acceptor, guarantor, amount, title, desc)
+            return {message: func}
+        }
     }
 }
